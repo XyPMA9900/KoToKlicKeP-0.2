a9a445ceea5122c3e2a3b96f9de45a0d35dfb66e
XyPMA9900 <arsenijkrisan52@gmail.com>
2026-01-23 22:30:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("SCRIPT LOADED");
+// alert("SCRIPT LOADED");
 document.addEventListener("DOMContentLoaded", () => {
 
   // ===== УТИЛИТА =====
@@ -35,15 +35,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const boostPriceEl = $("boostPrice");
   const critStatusEl = $("critStatus");
 
-  alert(
-  "score=" + !!scoreEl +
-  "\ncat=" + !!catBtn +
-  "\nopenShop=" + !!openShopBtn +
-  "\nshop=" + !!shopDiv +
-  "\nsettings=" + !!settingsDiv +
-  "\ndev=" + !!devDiv +
-  "\nupgrade=" + !!upgradeBtn
-);
+ // alert(
+//  "score=" + !!scoreEl +
+//  "\ncat=" + !!catBtn +
+//  "\nopenShop=" + !!openShopBtn +
+ // "\nshop=" + !!shopDiv +
+//  "\nsettings=" + !!settingsDiv +
+//  "\ndev=" + !!devDiv +
+//  "\nupgrade=" + !!upgradeBtn
+//);
 
   // ===== СОХРАНЕНИЕ =====
   const SAVE_KEY = "kotokliker_save";
